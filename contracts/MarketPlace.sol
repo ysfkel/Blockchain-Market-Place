3a9641b2e115506966b15b6383918da96c99e312
YUSUF <yusufkel@gmail.com>
2018-08-27 20:55:17 +0400

    added falback function

--- contracts/MarketPlace.sol
+++ contracts/MarketPlace.sol
@@ -20,5 +20,18 @@ UserManager, ShoppingCartManager, Withdrawable{
          tokenContract_shoppingCart = tokenContract;
         
     }
+
+    
+    /**
+      * @dev 
+      *Fallback function - Called if other functions don't match call or
+      * sent ether without data
+      * Typically, called when invalid data is sent
+      * Added so ether sent to this contract is reverted if the contract fails
+      * otherwise, the sender's money is transferred to contract
+      */
+    function () public{
+        revert();
+    }
 }
 
